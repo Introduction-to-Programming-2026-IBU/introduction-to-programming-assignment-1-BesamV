56209b9b6c7fb577644ce7312eeb6f4721e22901
Besam Veseli <besam.veseli@ibu.edu.mk>
2026-05-09 22:49:23 +0200

    Add files via upload

---
+++ week7/movies/12.sql
@@ -0,0 +1,13 @@
+SELECT title
+FROM movies
+WHERE id IN (
+    SELECT movie_id
+    FROM stars
+    JOIN people ON people.id = stars.person_id
+    WHERE name = 'Bradley Cooper'
+)
+AND id IN (
+    SELECT movie_id
+    FROM stars
+    JOIN people ON people.id = stars.person_id
+    WHERE name = 'Jennifer Lawrence'
---
+++ week7/movies/13.sql
@@ -0,0 +1,14 @@
+SELECT name
+FROM people
+WHERE id IN (
+    SELECT person_id
+    FROM stars
+    WHERE movie_id IN (
+        SELECT movie_id
+        FROM stars
+        JOIN people ON people.id = stars.person_id
+        WHERE name = 'Kevin Bacon'
+        AND birth = 1958
+    )
+)
+AND NOT (name = 'Kevin Bacon' AND birth = 1958);
